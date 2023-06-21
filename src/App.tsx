09bb8f72594e230e62d0c9355af28327f9d07ae4
Thalys Menezes <thalys.tauan@ulbra.inf.br>
2023-06-21 15:48:52 -0300

    starting repository

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { Header } from "./components/Header";
+
+
+
+
+export function App() {
+  return(
+    <div>"hello"</div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.scss";
+
+export function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>"hello"</h1>
+    </div>
+  );
+}
