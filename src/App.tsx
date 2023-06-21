6d19610dca7cc175aec1997cd0ebf8ea03a4a1ef
Thalys Menezes <thalys.tauan@ulbra.inf.br>
2023-06-21 16:12:51 -0300

    create files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { useState } from "react";
-import { Header } from "./components/Header";
 
-
-
-
-export function App() {
-  return(
-    <div>"hello"</div>
-  );
+interface ButtonProps {
+  tittle: string;
 }
+
+function Button(props: ButtonProps) {
+  return (
+    <button>
+        {props.tittle}
+    </button>
+  )
+}
+  
+
+
+function App(){
+  return (
+    <div>
+      <Button tittle="Button 1"/>
+      <Button tittle="Button 2"/>
+      <Button tittle="Button 3"/>
+      <Button tittle="Button 4"/>
+    </div>
+  )
+}
+
+export default App
+
