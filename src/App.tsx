adf6a7cd2a5776ba9f93860b2751ab544a42a648
unknown <leonardoriboliveira@gmail.com>
2023-06-22 16:33:20 -0300

    CHANGE FILES: Atualização da estilização dos botões; Criação da estrutura de cabeçalho e região descritiva para os números da rifa;

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
+import { Component } from 'react';
 import Button from '../src/components/Button'
 import './global.scss';
 
+
+
 function App(){
   return (
-    <div>
-      <Button tittle='Botão 1'/>
-      <Button tittle='Botão 2'/>
-      <Button tittle='Botão 3'/>
-      <Button tittle='Botão 4'/>
+    <div className="rifa-primary-content">
+      <div id="rifa-header">
+        <h2> Lista de Números </h2>
+        <div id="header-description">
+          <div className="number-status">
+            <div id="selected-numbers"></div>
+            <p> Indisponíveis </p>
+          </div>
+          <div className="number-status">
+            <div id="non-selected-numbers"></div>
+            <p> Disponíveis </p>
+          </div>
+        </div>
+      </div>
+      <div id="numbers-catalog">
+        <Button tittle='Número 1'/>
+        <Button tittle='Número 2'/>
+        <Button tittle='Número 3'/>
+        <Button tittle='Número 4'/>
+        <Button tittle='Número 5'/>
+      </div>
     </div>
   )
 }
